f73f9bf996a82aa7d0c8b1d7532b65c55c9c0368
宋鹏远 <turingmaster@163.com>
2020-04-23 23:31:04 +0800

    [title]：实现session

--- blogs_koa2/routes/user.js
+++ blogs_koa2/routes/user.js
@@ -8,10 +8,23 @@ router.prefix('/api/user')
 router.post('/login', async function (ctx, next) {
     const {username, password} = ctx.request.body;
     ctx.body = {
-        error: 0,
+        errno: 0,
         username,
         password
     }
 })
+
+
+router.get('/session-test', async function (ctx, next) {
+    if(ctx.session.viewCount == null){
+        ctx.session.viewCount = 0;
+    }
+    ctx.session.viewCount++
+    ctx.body = {
+        errno: 0,
+        viewCount: ctx.session.viewCount
+    }
+})
+
 
 module.exports = router
